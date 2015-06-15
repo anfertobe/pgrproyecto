1ed1dfda0f02b7665d7110161c5db4a9d38fdfa2
anfertobe <anfertobe@gmail.com>
2015-06-15 15:40:13 -0500

    pruebas

---
+++ src/test/java/pruebas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author andres
+ */
+public class pruebas {
+    
+ 
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void hello() 
+     {
+         assertTrue(true);
+     }
+}
